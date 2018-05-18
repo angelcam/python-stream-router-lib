1ab7798ccd1edb749fb459f3f2e64ddc09b78d80
Ondrej Perutka <ondrej.perutka@angelcam.com>
2018-05-18 11:01:43 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,22 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamrouter",
-    version='2.1.1',
+    version='3.0.0',
     description="Angelcam stream router library",
     keywords="asyncio stream router",
     author="Angelcam",
     author_email="dev@angelcam.com",
-    url="https://bitbucket.org/angelcam/python-stream-router-lib/",
+    url="https://github.com/angelcam/python-stream-router-lib",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "aiohttp >= 2.0.0",
-        "async_timeout >= 2.0.0",
-        "murmurhash3 >= 2.3.5",
-        "python-consul >= 0.7.0"
     ],
     dependency_links=[
-        "https://bitbucket.org/angelcam/python-hmac-tokens/get/v1.1.2.tar.gz#egg=hmac_tokens"
     ],
     tests_require=[
         "pytest",
