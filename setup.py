595be37cde1021abe6e8b8f07806ec5687ddf019
Ondrej Perutka <ondrej.perutka@angelcam.com>
2019-09-18 14:12:27 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamrouter",
-    version='3.0.0',
+    version='5.0.0',
     description="Angelcam stream router library",
     keywords="asyncio stream router",
     author="Angelcam",
